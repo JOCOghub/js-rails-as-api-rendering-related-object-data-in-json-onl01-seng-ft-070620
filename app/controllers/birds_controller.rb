28445ce5c7d5f135477c2a6641f9c99a89e4c39b
Joseph cohen <jojocohen57@gmail.com>
2020-10-16 21:09:04 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,5 +7,5 @@ class BirdsController < ApplicationController
   def show
   sighting = Sighting.find_by(id: params[:id])
   render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
-end
+  end
 end
